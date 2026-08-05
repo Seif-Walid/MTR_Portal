89e803bf9d2d2c7fbfd3d4b19b112a51d9cd53eb
Seif Walid <seifwalid64@gmail.com>
2026-08-05 04:45:16 +0300

    Delete functions/api directory

--- functions/api/[[path]].ts
+++
@@ -1,9 +0,0 @@
-export async function onRequest(context) {
-  const url = new URL(context.request.url);
-  url.hostname = "mtr.tail4bd79c.ts.net";
-  url.protocol = "https:";
-  url.port = "443";
-
-  // Natively clones and forwards the method, headers, and body without throwing exceptions
-  return fetch(new Request(url.toString(), context.request));
-}
